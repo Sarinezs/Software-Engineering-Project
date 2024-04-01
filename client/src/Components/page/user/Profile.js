4fa333691ea1b893ff5a9d00e544b2a29f783490
Suriya Meerua <a.edr.edm.ed@gmail.com>
2024-04-02 00:15:47 +0700

    .

--- client/src/Components/page/user/Profile.js
+++ client/src/Components/page/user/Profile.js
@@ -8,8 +8,6 @@ import Button from '@mui/material/Button';
 
 
 const Profile = () => {
-<<<<<<< Updated upstream
-=======
     const navi = useNavigate()
 
     const c_user = useSelector((state) => ({ ...state }))
@@ -19,7 +17,6 @@ const Profile = () => {
         navi("/" + name)
     }
 
->>>>>>> Stashed changes
     return (
         <div style={{ marginBottom: "100%" }}>
 
@@ -50,18 +47,9 @@ const Profile = () => {
                                 </a>
                             </li>
                             <li>
-<<<<<<< Updated upstream
-                                <a href="/account">
-                                    <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" />
-                                        <path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z" />
-                                    </svg>
-
-                                </a>
-=======
                                 <svg onClick={() => { warp("account") }} xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24"><path d="M0 0h24v24H0z" fill="none" />
                                     <path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z" />
                                 </svg>
->>>>>>> Stashed changes
                             </li>
                             <li>
                                 <svg onClick={() => { warp("cart") }} class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
